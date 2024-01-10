19508ab185636666f023fc20a2e412ae9c6c4660
danieltakacs1 <dani.takacs2002@gmail.com>
2024-01-10 10:28:21 +0100

    elso

---
+++ src/test/java/rombTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class rombTest {
+
+    Romb romb;
+
+    @BeforeTest
+    public void testCalc() {
+        this.romb = new Romb();
+    }
+
+    @Test
+    public void
+}
